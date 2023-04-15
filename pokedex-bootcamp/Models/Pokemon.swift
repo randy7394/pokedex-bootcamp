582891f8201347807911f6f91a489ff05df02ea1
derian-all-win-software <derian@all-win-software>
2023-04-14 19:56:22 -0600

    feat: Added the Pokemon model

---
+++ pokedex-bootcamp/Models/Pokemon.swift
@@ -0,0 +1,18 @@
+//
+//  Pokemon.swift
+//  pokedex-bootcamp
+//
+//  Created by Aylwing Olivas on 4/14/23.
+//
+
+import Foundation
+
+struct Pokemon {
+    let name: String
+    let type: String
+}
+
+let pokemons: [Pokemon] = [
+    Pokemon(name: "Charmander", type: "Fire"),
+    Pokemon(name: "Squirtle", type: "Water")
+]
