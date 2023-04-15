9bc37a961640a8d35bc35e72f678a3f0153b46e5
derian-all-win-software <derian@all-win-software>
2023-04-14 19:34:06 -0600

    feat: Added the font to Label

--- pokedex-bootcamp/ViewController.swift
+++ pokedex-bootcamp/ViewController.swift
@@ -8,9 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet private var welcomeTitle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUI()
+    }
+    
+    private func setUI() {
+        welcomeTitle.font = UIFont.systemFont(ofSize: 34)
     }
 }
 
