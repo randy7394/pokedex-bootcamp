6ad12d9a9f39122b0a1966d3f80e13cbabaea25e
derian-all-win-software <derian@all-win-software>
2023-04-14 19:20:39 -0600

    feat: First commit

---
+++ pokedex-bootcamp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  pokedex-bootcamp
+//
+//  Created by Derian Córdoba on 4/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+    }
+
+    private func setUI() {
+        
+    }
+}
+
