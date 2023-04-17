d591bfb6062db08f0074c7d1034e781376a529eb
randy7394 <randymorales17@gmail.com>
2023-04-17 11:56:42 -0600

    Feat: 10 Pokemon added to the list

--- pokedex-bootcamp/Models/Pokemon.swift
+++ pokedex-bootcamp/Models/Pokemon.swift
@@ -14,5 +14,15 @@ struct Pokemon {
 
 let pokemons: [Pokemon] = [
     Pokemon(name: "Charmander", type: "Fire"),
-    Pokemon(name: "Squirtle", type: "Water")
+    Pokemon(name: "Squirtle", type: "Water"),
+    Pokemon(name: "Bulbasur", type: "Grass"),
+    Pokemon(name: "Caterpie", type: "Bug"),
+    Pokemon(name: "Weedle", type: "Bug"),
+    Pokemon(name: "Pidgey", type: "Flying"),
+    Pokemon(name: "Rattata", type: "Normal"),
+    Pokemon(name: "Ekans", type: "Poison"),
+    Pokemon(name: "Pikachu", type: "Electric"),
+    Pokemon(name: "Sandshrew", type: "Ground"),
+    Pokemon(name: "Nidoran", type: "Poison"),
+    Pokemon(name: "Vulpix", type: "Fire")
 ]
